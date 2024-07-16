afdaf4a7c5fc7b03ca31c45cf2c4aa43c045946b
Tarun08091997 <Tarunsharma080997~gmail.com>
2024-07-16 23:59:15 +0530

    routing started DataBase remaining

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv');
 
-const app = express();
+// Route Imports
+const product = require("./routes/productRouter");
+
 
 dotenv.config({path:'./config/config.env'})
+
+const app = express();
+app.use(express.json());  // body parser
+
+
+// Routing
+app.use("/api/v1",product);
 
 module.exports = app;
---
+++ backend/controller/productController.js
@@ -0,0 +1,3 @@
+exports.getAllProducts = (req,res,next)=>{
+    res.status(200).send({msg:"Routing working fine"});
+}
